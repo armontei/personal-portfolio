133ca363ab6ced65c40ee6b8101f510bfccfd7e9
Amanda Monteiro <a3monteiro@gmail.com>
2020-12-15 13:08:48 -0500

    more sections and styles  :alien:

--- script.js
+++ script.js
@@ -4,3 +4,37 @@
 // website images inside screen
 // nav fixed
 // contact icons fixed
+// social media links in bubbles inside header
+
+// let skillHover = document.getElementsByClassName("skills-label");
+
+// for (let i = 0; i < skillHover.length; i++) {
+//     skillHover[i].addEventListener("mouseenter", function (event) {
+//         // highlight the mouseenter target
+//         console.log("hover");
+//         event.target.style.opacity = 1;
+
+//     }, false);
+// }
+
+(function () {
+
+    let skillHover = document.getElementsByClassName("skills-label");
+
+    for (let i = 0; i < skillHover.length; i++) {
+
+        function showDiv(e) {
+            e.target.style.opacity = 1;
+        }
+    
+        function hideDiv(e) {
+            e.target.style.opacity = 0;
+        }
+    
+        skillHover[i].onmouseover = showDiv;
+        skillHover[i].onmouseleave = hideDiv;
+    }
+
+
+})();
+
