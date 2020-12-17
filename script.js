27815f0d9b067cb02e91d72e7c5a4c0e02ead058
Amanda Monteiro <a3monteiro@gmail.com>
2020-12-17 18:07:59 -0500

    changed background, some form styles, footer  :octopus: :octopus:

--- script.js
+++ script.js
@@ -29,6 +29,31 @@ function downHandler() {
     });
 }
 
+// var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+// var rootElement = document.documentElement
+
+// function handleScroll() {
+//     // do something on scroll
+//     var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+//     if ((rootElement.scrollTop / scrollTotal) > 0.80) {
+//         //show button
+//         scrollToTopBtn.style.display = "block"
+//     } else {
+//         //hide button
+//         scrollToTopBtn.style.display = "none"
+//     }
+// }
+
+// function scrollToTop() {
+//     //scroll to top logic
+//     rootElement.scrollTo({
+//         top: 0,
+//         behavior: "smooth"
+//     })
+// }
+// scrollToTopBtn.addEventListener("click", scrollToTop)
+// document.addEventListener("scroll", handleScroll)
+
 // Fade-in animation initialize
 AOS.init();
 
